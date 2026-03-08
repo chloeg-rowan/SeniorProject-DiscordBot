3a7f748c19fb5a63fe4645e56b131925887ababf
chloeg-rowan <c.garvey.2023@gmail.com>
2026-03-08 19:31:35 -0400

    Basic bot functionality implemented, still need to figure out the chess setup

--- main.py
+++ main.py
@@ -12,7 +12,38 @@ client = discord.Client(intents = intents)
 
 load_dotenv()
 
+@client.event
+async def on_ready():
+    print(f'Signed on as {client.user}')
 
 
+@client.event
+async def on_message(message):
+    
+    if(message.content.startswith("!Help")) or (message.content.startswith("!help")):
+        await message.channel.send("Commands: " 
+                                   + "\n\n**!Login** - Log in to your account.")
+        
+    if(message.content.startswith("!Login")) or (message.content.startswith("!login")): #Once the database is functional, all methods will need to check if they logged in.
+        await message.channel.send("Please enter your username. (Still in development, use \"admin\" as the username and \"password\" as the password.)")
+        validEntry = False
+        while not validEntry:
+            mess = await client.wait_for("message", check=lambda msg: msg.author == message.author, timeout = 300.0)
+            #Communicate with the database to check for login data. For now we will just check if the username is "admin" and the password is "password"
+            if(mess.content == "admin"):
+                validEntry = True
+            else:
+                await message.channel.send("Invalid username. Please enter your username.")
+        await message.channel.send("Please enter your password.")
+        validEntry = False
+        while not validEntry:
+            mess = await client.wait_for("message", check=lambda msg: msg.author == message.author, timeout = 300.0)
+            if(mess.content == "password"):
+                validEntry = True
+            else:
+                await message.channel.send("Invalid password. Please enter your password.")
+        await message.channel.send("Login successful!")
+        
+    
 
 client.run(os.getenv('DISCORD_BOT_TOKEN'))
